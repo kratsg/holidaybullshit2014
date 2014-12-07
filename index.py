c2d7d5eef879f477a3ea4070c9f198306810d7d5
Giordon Stark <kratsg@gmail.com>
2014-12-07 13:50:32 -0600

    Added caching with mongo

--- index.py
+++ index.py
@@ -2,10 +2,14 @@ import os
 from flask import Flask, render_template, redirect, url_for
 import requests
 
+import pymongo
+
 import logging
 from logging import FileHandler
 
 app = Flask(__name__)
+
+MONGO_URL = os.environ.get('MONGOHQ_URL')
 
 def is_number(s):
     try:
@@ -13,6 +17,36 @@ def is_number(s):
         return True
     except ValueError:
         return False
+
+def dbconn():
+  if MONGO_URL:
+    conn = pymongo.Connection(MONGO_URL)
+    # get the database
+    db = conn[urlparse(MONGO_URL).path[1:]]
+  else:
+    conn = pymongo.Connection('localhost', 27017)
+    db = conn['test']
+  return db
+
+def testdb():
+  db = dbconn()
+  db.test_collection.insert({"testdoc":"totaltest"})
+  print db.test_collection.find()[0]
+
+def get_cache(phrase):
+  db = dbconn()
+  res = db.phrases.find_one({ phrase: {"$exists": "true"} })
+  if res:
+    # exists
+    return res[phrase]
+  else:
+    # does not exist
+    return None
+
+def set_cache(phrase, imageID):
+  db = dbconn()
+  res = db.phrases.insert({phrase: imageID})
+  return True
 
 @app.route('/')
 def index():
@@ -27,15 +61,25 @@ def run(phrase):
     return redirect(url_for('.index'))
   # normalize the phrase
   phrase = phrase.lower().replace(' ','')
-  payload = {'q': phrase}
-  # make request to get json, ignore SSL stuff
-  r = requests.get('https://ask.cardsagainsthumanity.com/single', params=payload, verify=False)
-  try:
-    imageID = r.json()['a']
-    r = recordResult(phrase, imageID)
-  except:
-    pass
-  return render_template('index.html', phrase=phrase, imageID=imageID, status=r.status_code)
+
+  # check if we've cached it
+  cache = get_cache(phrase)
+  if cache:
+    imageID = cache
+    status_code = 'cache'
+  else:
+    payload = {'q': phrase}
+    # make request to get json, ignore SSL stuff
+    r = requests.get('https://ask.cardsagainsthumanity.com/single', params=payload, verify=False)
+    try:
+      imageID = r.json()['a']
+      r = recordResult(phrase, imageID)
+      status_code = r.status_code
+      # log in the cache for future reference
+      set_cache(phrase, imageID)
+    except:
+      return redirect(url_for('.index'))
+  return render_template('index.html', phrase=phrase, imageID=imageID, status=status_code)
 
 
 def recordResult(phrase, imageID):
