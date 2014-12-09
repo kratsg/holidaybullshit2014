e1e99da1f1ccaa6b2d9063009c19ea8f7511efa0
Giordon Stark <kratsg@gmail.com>
2014-12-09 17:30:52 -0600

    link to search on reddit. Added a counter to count how many times it's been requested before

--- index.py
+++ index.py
@@ -41,14 +41,18 @@ def get_cache(phrase):
   res = db.phrases.find_one({ phrase: {"$exists": "true"} })
   if res:
     # exists
-    return res[phrase]
+    db.phrases.update({"_id": res['_id']}, {"$inc": {"requests": 1}})
+    try:
+      return res[phrase], res['requests']
+    except:
+      return res[phrase], 1
   else:
     # does not exist
-    return None
+    return None, 0
 
 def set_cache(phrase, imageID):
   db = dbconn()
-  res = db.phrases.insert({phrase: imageID})
+  res = db.phrases.insert({phrase: imageID, "requests": 1})
   return True
 
 @app.route('/')
@@ -84,10 +88,11 @@ def run(phrase):
   phrase = phrase.lower().replace(' ','')
 
   # check if we've cached it
-  cache = get_cache(phrase)
+  cache, counts = get_cache(phrase)
   if cache:
     imageID = cache
     status_code = 'cache'
+    num_requests = counts
   else:
     payload = {'q': phrase}
     # make request to get json, ignore SSL stuff
@@ -96,15 +101,15 @@ def run(phrase):
       imageID = r.json()['a']
       r = recordResult(phrase, imageID)
       status_code = r.status_code
+      num_requests = 0
       # log in the cache for future reference
       set_cache(phrase, imageID)
     except:
       return redirect(url_for('.index'))
-  return render_template('index.html', phrase=phrase, imageID=imageID, status=status_code)
+  return render_template('index.html', phrase=phrase, imageID=imageID, status=status_code, num_requests=num_requests)
 
 
 def recordResult(phrase, imageID):
-  print "we are here!"
   # http://mikeheavers.com/main/code-item/submitting_custom_form_data_to_a_google_spreadsheet_with_javascript
   formURL = "https://docs.google.com/forms/d/1J4BIqr06ZgOse-Eo7YpwGbdr3xRyIohgUSW2uq2iatk/formResponse"
   imageID_textbox = "entry.1503938973"
